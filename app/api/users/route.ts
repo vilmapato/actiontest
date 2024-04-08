674b78c719ddb36e259f0322b48b49720fb1fd6e
Vilma <vilmapati@hotmail.com>
2024-04-08 22:10:33 +0200

    testing api/user

---
+++ app/api/users/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from '../../../prisma/client';
+
+const createUserSchema = z.object({
+    name: z.string().min(1).max(255),
+    email: z.string().min(1).max(255),
+    image: z.string().min(1)
+})
+export async function POST(request:NextRequest) {
+    const body =await request.json() as { name: string, email: string };
+    const validation = createUserSchema.safeParse(body)
+    if (!validation.success) 
+        return NextResponse.json(validation.error.errors, {status : 400})
+
+    const newUser = await prisma.user.create({
+        data: { name: body.name, email: body.email}
+    })
+    
+}
